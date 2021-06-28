163de2b3ee4506a80ea543a1308a6444063c0c2f
Vlad <vlad.setchin@gmail.com>
2021-06-29 08:13:51 +1000

    create nx workspace and blog project

---
+++ apps/blog/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  displayName: 'blog',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../coverage/apps/blog',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+  },
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
