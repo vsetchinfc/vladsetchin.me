f9ced6742395fcbdcc9947ad994396ab333b83a2
Vlad <vlad.setchin@gmail.com>
2021-06-29 11:44:24 +1000

    Added main-page component  and routing module

--- apps/blog/src/app/app.module.ts
+++ apps/blog/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './containers/main-page/main-page.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, MainPageComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ apps/blog/src/app/containers/index.ts
@@ -0,0 +1,5 @@
+export * from './main-page/main-page.component';
+
+import { MainPageComponent } from './main-page/main-page.component';
+
+export const CONTAINERS = [MainPageComponent];
