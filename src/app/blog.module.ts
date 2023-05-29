90d2cb3d05d9bc7b1f56dfe9fa9d5ba0e4c8a46a
Vlad Setchin <vlad.setchin@gmail.com>
2023-05-29 22:29:42 +1000

    BLOG-1: scaffolded header and footer components

--- src/app/blog.module.ts
+++ src/app/blog.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BlogRoutingModule } from './blog-routing.module';
 import { BlogComponent } from './blog.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    BlogComponent
+    BlogComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
