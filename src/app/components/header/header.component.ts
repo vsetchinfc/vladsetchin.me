75ef1cc4c7349a368fdd310ba32be9131fb778bf
Vlad Setchin <vlad.setchin@gmail.com>
2023-06-06 20:03:33 +1000

    BLOG-1: Implemented Header Component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
+  get blogName() {
+    return 'VLADSETCHIN.ME'; //this.blogConfigService.blogName;
+  }
 }
